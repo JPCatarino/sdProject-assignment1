7c8f82ae249823cc40bd6537287a0a00c9509518
JPCatarino <jorge.catarino@ua.pt>
2020-03-04 18:52:15 +0000

    [ENTITIES] Add interface for 3 base entities

---
+++ src/entities/BusDriverInterface.java
@@ -0,0 +1,38 @@
+package entities;
+
+/**
+ * Interface for the Bus Driver entity.
+ * This interface contains the signatures of the functions to be instantiated on
+ * shared memory regions.
+ *
+ * @author Jorge Catarino
+ * @author Fabio Alves
+ */
+
+public interface BusDriverInterface {
+
+    /**
+     * announcingBusBoarding
+     */
+    public void announcingBusBoarding ();
+
+    /**
+     * goToDepartureTerminal
+     */
+    public void goToDepartureTerminal ();
+
+    /**
+     * parkTheBusAndLetPassOff
+     */
+    public void parkTheBusAndLetPassOff ();
+
+    /**
+     * goToArrivalTerminal
+     */
+    public void goToArrivalTerminal ();
+
+    /**
+     * parkTheBus
+     */
+    public void parkTheBus ();
+}
