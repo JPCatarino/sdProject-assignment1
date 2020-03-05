0dd42819845508ad72144fae0dae2a3b4afd70b4
JPCatarino <jorge.catarino@ua.pt>
2020-03-05 10:28:07 +0000

    [INTERFACES] Create interface package

--- src/interfaces/PassengerInterface.java
+++ src/interfaces/PassengerInterface.java
@@ -1,4 +1,4 @@
-package entities;
+package interfaces;
 
 /**
  * Interface for the Passenger entity.
--- src/interfaces/PorterInterface.java
+++ src/interfaces/PorterInterface.java
@@ -1,4 +1,4 @@
-package entities;
+package interfaces;
 
 /**
  * Interface for the Porter entity.
