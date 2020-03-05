6126c4476338bc1c73d974a7cb1ec680ae7102e3
fabioal0 <fabioal0@hotmail.com>
2020-03-05 09:52:30 +0000

    Update enumerates

--- src/states/BusDriverStates.java
+++ src/states/BusDriverStates.java
@@ -1,8 +1,17 @@
 package states;
 
 public enum BusDriverStates {
-    PARKING_AT_THE_ARRIVAL_TERMINAL,
-    DRIVING_FORWARD,
-    PARKING_AT_THE_DEPARTURE_TERMINAL,
-    DRIVING_BACKWARD
-}
+    PARKING_AT_THE_ARRIVAL_TERMINAL(""),
+    DRIVING_FORWARD(""),
+    PARKING_AT_THE_DEPARTURE_TERMINAL(""),
+    DRIVING_BACKWARD("");
+
+    BusDriverStates(String state) {
+        this.state = state;
+    }
+
+    private String state;
+
+    public String getState() {
+        return state;
+    }
--- src/states/PorterStates.java
+++ src/states/PorterStates.java
@@ -1,8 +1,18 @@
 package states;
 
 public enum PorterStates {
-    WAITING_FOR_A_PLANE_TO_LAND,
-    AT_THE_PLANES_HOLD,
-    AT_THE_LUGGAGE_BELT_CONVEYOR,
-    AT_THE_STOREROOM
+    WAITING_FOR_A_PLANE_TO_LAND(""),
+    AT_THE_PLANES_HOLD(""),
+    AT_THE_LUGGAGE_BELT_CONVEYOR(""),
+    AT_THE_STOREROOM("");
+
+    PorterStates(String state) {
+        this.state = state;
+    }
+
+    private String state;
+
+    public String getState() {
+        return state;
+    }
 }
