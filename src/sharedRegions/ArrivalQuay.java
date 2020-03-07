05fd7e49337241a859b1d394e42886048e0a74c9
JPCatarino <jorge.catarino@ua.pt>
2020-03-07 17:20:22 +0000

    [SHAREG] Add announcingBusBorading to BusDriver

--- src/sharedRegions/ArrivalQuay.java
+++ src/sharedRegions/ArrivalQuay.java
@@ -1,18 +1,43 @@
 package sharedRegions;
 
+import entities.BusDriver;
+import entities.Passenger;
 import interfaces.ATTQBusDriver;
 import interfaces.ATTQPassenger;
+
+import java.util.Queue;
 
 public class ArrivalQuay implements ATTQBusDriver, ATTQPassenger {
 
     Repository repo;
+    Queue<Passenger> busWaitingLine;
+    int timeForDeparture;
 
     public ArrivalQuay(Repository repo){
         this.repo = repo;
     }
 
+    /**
+     * Annoucing the boarding, the Bus Driver is letting the Passenger in the queue know it's okay
+     * to get inside the bus.
+     */
     @Override
-    public synchronized void announcingBusBoarding(){}
+    public synchronized void announcingBusBoarding(){
+        BusDriver bd = (BusDriver)Thread.currentThread();
+        try {
+            while (busWaitingLine.size() != repo.getT_SEATS()) {            // TODO: Add departure time as a condition
+                wait();
+            }
+
+            while(!busWaitingLine.isEmpty()){
+                notifyAll();                                                // Notify passengers for them to enter the bus.
+                wait();                                                     // Block while passengers enter bus;
+            }
+        }
+        catch(InterruptedException iex){
+            System.err.println("announcingBusBoarding - Thread was interrupted.");
+        }
+    }
 
     @Override
     public synchronized void goToDepartureTerminal(){}
--- src/sharedRegions/Repository.java
+++ src/sharedRegions/Repository.java
@@ -1,4 +1,17 @@
 package sharedRegions;
 
 public class Repository {
+    /**
+     * Number of landings for this simulation.
+     */
+    public int K_LANDINGS = 6;
+
+    /**
+     * Capacity of the transfer bus.
+     */
+    public int T_SEATS = 3;
+
+    public int getT_SEATS() {
+        return T_SEATS;
+    }
 }
