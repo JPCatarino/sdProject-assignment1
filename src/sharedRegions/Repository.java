3738e1e179db7df4706b1e71688a3ccd849611de
JPCatarino <jorge.catarino@ua.pt>
2020-03-14 15:33:40 +0000

    [REPO] Add global variables

--- src/sharedRegions/Repository.java
+++ src/sharedRegions/Repository.java
@@ -4,12 +4,34 @@ public class Repository {
     /**
      * Number of landings for this simulation.
      */
-    public int K_LANDINGS = 6;
+    private int K_LANDINGS = 5;
+
+    /**
+     * Number of passengers for this simulation.
+     */
+    private int N_PASSENGERS = 6;
+
+    /**
+     * Number of luggage allowed on plane hold for this simulation.
+     */
+    private int M_LUGGAGE = 2;
 
     /**
      * Capacity of the transfer bus.
      */
-    public int T_SEATS = 3;
+    private int T_SEATS = 3;
+
+    public int getK_LANDINGS() {
+        return K_LANDINGS;
+    }
+
+    public int getN_PASSENGERS() {
+        return N_PASSENGERS;
+    }
+
+    public int getM_LUGGAGE() {
+        return M_LUGGAGE;
+    }
 
     public int getT_SEATS() {
         return T_SEATS;
