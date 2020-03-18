448198f07c892cd63e08c676a931bf00e12a0b30
fabioal0 <fabioal0@hotmail.com>
2020-03-18 15:22:51 +0000

    [STATES] Enum add description

--- src/states/PorterStates.java
+++ src/states/PorterStates.java
@@ -1,10 +1,10 @@
 package states;
 
 public enum PorterStates {
-    WAITING_FOR_A_PLANE_TO_LAND(""),
-    AT_THE_PLANES_HOLD(""),
-    AT_THE_LUGGAGE_BELT_CONVEYOR(""),
-    AT_THE_STOREROOM("");
+    WAITING_FOR_A_PLANE_TO_LAND("WPTL"),
+    AT_THE_PLANES_HOLD("APLH"),
+    AT_THE_LUGGAGE_BELT_CONVEYOR("ALCB"),
+    AT_THE_STOREROOM("ASTR");
 
     PorterStates(String state) {
         this.state = state;
