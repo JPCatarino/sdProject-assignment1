8a179a2820684c720fd4180a956f8ed26c9010ab
JPCatarino <jorge.catarino@ua.pt>
2020-03-25 23:49:54 +0000

    [SHAREG] BagColPoint - goCollectABag

--- src/sharedRegions/BagColPoint.java
+++ src/sharedRegions/BagColPoint.java
@@ -6,13 +6,16 @@ import interfaces.BCPPorter;
 import states.PassengerStates;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class BagColPoint implements BCPPassenger, BCPPorter {
 
     private Repository repo;
-    private List<Arrays> conveyorBelt;
+    private List<int[]> conveyorBelt;
+
+    private boolean noMoreBags;
+
+    private boolean bagsInTheConveyorBelt;
 
     public BagColPoint(Repository repo){
         this.repo = repo;
@@ -24,7 +27,28 @@ public class BagColPoint implements BCPPassenger, BCPPorter {
         Passenger p = (Passenger) Thread.currentThread();
         p.setPassengerState(PassengerStates.AT_THE_LUGGAGE_COLLECTION_POINT);
 
-
+        // Logic may be changed
+        // While there's bags on the hold the passenger waits
+        // If there's bags on the conveyor belt it tries to collect one with it's ID
+        // In case the passengers find one, it collects it.
+        try{
+            while(!noMoreBags){
+                if(bagsInTheConveyorBelt){
+                    for(int i = 0; i < conveyorBelt.size(); i++){
+                        if(conveyorBelt.get(i)[0] == p.getID()){
+                            conveyorBelt.remove(i);
+                            p.collectedABag();
+                            break;
+                        }
+                    }
+                }
+                else
+                    wait();
+            }
+        }
+        catch(InterruptedException ex){
+            System.err.println("goCollectABag - Thread Interrupted");
+        }
 
     };
 
